58051a37f54d82a02c9759a835c83d44b2207166
-akku- <151517253+akku1139@users.noreply.github.com>
2024-05-23 12:54:38 +0900

    b

--- scripts/main.ts
+++ scripts/main.ts
@@ -6,8 +6,9 @@ let posts: WP_REST_API_Posts = await firstRes.json();
 const totalPages = Number(firstRes.headers.get("X-Wp-Totalpages"));
 
 for(let i = 2; i < (totalPages + 1); i++) {
-  console.log("page");
   posts.concat(await (await fetch("https://blogbooks.net/wp-json/wp/v2/posts?page=1&per_page=100")).json());
 }
 
-console.log(posts);
+posts.forEach((post) => {
+  Deno.writeTextFile(`./raw/${post.id}`, JSON.stringify(post));
+});
