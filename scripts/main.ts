1145bce19ce842799d309c5e509a664da3d4d416
-akku- <151517253+akku1139@users.noreply.github.com>
2024-05-23 15:05:38 +0900

    ループ

--- scripts/main.ts
+++ scripts/main.ts
@@ -6,7 +6,7 @@ let posts: WP_REST_API_Posts = await firstRes.json();
 const totalPages = Number(firstRes.headers.get("X-Wp-Totalpages"));
 
 for(let i = 2; i < (totalPages + 1); i++) {
-  posts.concat(await (await fetch("https://blogbooks.net/wp-json/wp/v2/posts?page=1&per_page=100")).json());
+  posts.concat(await (await fetch(`https://blogbooks.net/wp-json/wp/v2/posts?page=${i}&per_page=100`)).json());
 }
 
 posts.forEach((post) => {
