9dd28b3e7879c6123eee44b65b758c58fa5d4c71
-akku- <151517253+akku1139@users.noreply.github.com>
2024-05-23 12:00:32 +0900

    a

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,1 +1,10 @@
+import type { WP_REST_API_Posts } from "./types/wp.ts";
 
+const firstRes = await fetch("https://blogbooks.net/wp-json/wp/v2/posts?page=1&per_page=100");
+let posts: WP_REST_API_Posts = await firstRes.json();
+
+let totalCount = Number(firstRes.headers.get("X-WP-Total"));
+
+while(totalCount > 0) {
+
+}
