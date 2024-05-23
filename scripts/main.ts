fa766502f97ece5f98d1e0494dc54c227dea4fcc
-akku- <151517253+akku1139@users.noreply.github.com>
2024-05-23 12:45:43 +0900

    a

--- scripts/main.ts
+++ scripts/main.ts
@@ -5,7 +5,9 @@ let posts: WP_REST_API_Posts = await firstRes.json();
 
 const totalPages = Number(firstRes.headers.get("X-Wp-Totalpages"));
 
-for(let i = 2; i < totalPages+1; i++) {
+for(let i = 2; i < (totalPages + 1); i++) {
   console.log("page");
-  posts.await (await fetch("https://blogbooks.net/wp-json/wp/v2/posts?page=1&per_page=100")).json();
+  posts.concat(await (await fetch("https://blogbooks.net/wp-json/wp/v2/posts?page=1&per_page=100")).json());
 }
+
+console.log(posts);
