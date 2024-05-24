6fa7a17e6a349acbf64d6d4d657bb96b474052ee
-akku- <151517253+akku1139@users.noreply.github.com>
2024-05-24 12:06:38 +0900

    分離

--- scripts/main.ts
+++ scripts/main.ts
@@ -34,6 +34,7 @@ posts.forEach((post) => {
   (post.content.rendered.match(/"https:\/\/blogbooks\.net\/wp-content\/(.+?)"/g) ?? []).map((u) => u.slice(1, -1)).forEach(async (url) => {
     const path = "./static" + new URL(url).pathname;
     await Deno.mkdir(path, {recursive: true});
-    Deno.writeFile(path, await ((await fetch(url)).body()), {write: true, createNew: true}).catch(console.log);
+    const data = await (await fetch(url)).body();
+    Deno.writeFile(path, data, {write: true, createNew: true}).catch(console.log);
   });
 });
